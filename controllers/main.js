a0869d93df96e2f3a718e817f84ff324360fb571
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-26 21:37:06 +0530

    creating tokens

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,14 @@
 const CustomAPIError = require("../errors/custom-error");
-
+const jwt = require('jsonwebtoken')
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     throw new CustomAPIError("please provide email id and password", 400);
   }
+  const id = new Date().getDate();
+  const token = jwt.sign({id, username },process.env.JWT_SECRET,{expiresIn:'30d'});
 
-  console.log(username, password);
-  res.send("Fake Login/Register/Signup route");
+  res.status(200).json({msg:'user created',token})
 };
 
 const dashboard = async (req, res) => {
