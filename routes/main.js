b371ab6b274a1283827b30fa97431d2c93087e17
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-27 20:09:11 +0530

    error handling

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { login, dashboard } = require("../controllers/main");
 
-const authMiddleware = require('../middleware/auth')
+const authMiddleware = require('../errors/auth')
 
 router.route("/dashboard").get(authMiddleware,dashboard);
 router.route("/login").post(login);
