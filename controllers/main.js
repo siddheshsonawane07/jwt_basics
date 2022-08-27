9946eb9ad79c69d10c9432206e5916078f44a089
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-27 20:13:49 +0530

    adding middleware to main.js

--- controllers/main.js
+++ controllers/main.js
@@ -1,31 +1,30 @@
-require("dotenv").config();
-const CustomAPIError = require("../errors/custom-error");
 const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+
 const login = async (req, res) => {
   const { username, password } = req.body;
+
   if (!username || !password) {
-    throw new CustomAPIError("please provide email id and password", 400);
+    throw new BadRequestError("Please provide email and password");
   }
+
   const id = new Date().getDate();
+
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 
-  res.status(200).json({ msg: "user created", token: token });
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization
+  const luckyNumber = Math.floor(Math.random() * 100);
 
-  if (!authHeader || !authHeader.startWith("Bearer ")) {
-    throw new CustomAPIError("No token provided", 400);
-  }
-
-  const token = authHeader.split(" "[1]);
-
-  
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
-
 module.exports = {
   login,
   dashboard,
