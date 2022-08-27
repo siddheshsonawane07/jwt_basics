5372d126128b4404fd7b985ce0892d74ccfe8e31
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-08-27 16:38:31 +0530

    saving jwt generated from login route in dashboard

--- controllers/main.js
+++ controllers/main.js
@@ -1,17 +1,20 @@
 const CustomAPIError = require("../errors/custom-error");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
     throw new CustomAPIError("please provide email id and password", 400);
   }
   const id = new Date().getDate();
-  const token = jwt.sign({id, username },process.env.JWT_SECRET,{expiresIn:'30d'});
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 
-  res.status(200).json({msg:'user created',token})
+  res.status(200).json({ msg: "user created", token: token });
 };
 
 const dashboard = async (req, res) => {
+  console.log(req.headers);
   const luckynumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello Siddhesh`,
